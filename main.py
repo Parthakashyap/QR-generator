03a579e8ee263fef9e7a9453ee91c91b73fbc4f5
Partha Kashyap <parthakashyal@gmail.com>
2024-04-04 10:57:59 +0530

    commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 import pyqrcode
 import png
 from pyqrcode import QRCode
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -9,10 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 
 def QrCode():
-    s   =  input()
+    s   =  ("www.google.com")
     url = pyqrcode.create(s)
     url.png('myqr.png',scale=6)
-    return "<img src='myqr.png'/>"
+    return render_template("index.html", QrCode=url)
 
 
 if __name__=='__main__':
