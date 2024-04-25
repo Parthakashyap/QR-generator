36a6b9bbaa74cb5f379d9619ad99d98df47bb3ff
Partha Kashyap <parthakashyal@gmail.com>
2024-04-25 12:48:26 +0530

    deployment fix

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, request
 import pyqrcode
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    @app.route('/')
+    def index():
+        return render_template("index.html", url="")
 
-@app.route('/')
-def index():
-    return render_template("index.html", url="")
+    @app.route('/generate', methods=['POST'])
+    def generate():
+        if request.method == 'POST':
+            site = request.form['site']
+            if site:
+                url = pyqrcode.create(site)
+                # Save the QR code as a temporary file (or in memory) to serve to the frontend
+                # For simplicity, we'll encode the QR code image as a data URI directly in HTML
+                qr_as_base64 = url.png_as_base64_str(scale=6)
+                return qr_as_base64
+        return ''
+    
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    if request.method == 'POST':
-        site = request.form['site']
-        if site:
-            url = pyqrcode.create(site)
-            # Save the QR code as a temporary file (or in memory) to serve to the frontend
-            # For simplicity, we'll encode the QR code image as a data URI directly in HTML
-            qr_as_base64 = url.png_as_base64_str(scale=6)
-            return qr_as_base64
-    return ''
-
+    return app
 
 
